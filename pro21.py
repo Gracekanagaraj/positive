ff33d28de276ec3bfe71b590fc7f50ce1dba7940
Gracekanagaraj <52075080+Gracekanagaraj@users.noreply.github.com>
2019-07-19 11:34:24 +0530

    Update pro21.py

--- pro21.py
+++ pro21.py
@@ -1,6 +1,6 @@
 nn1=int(input())
 li = list(map(int,input().split()))
-ll1 = sum(li[:len(l)//2])/len(li[:len(l)//2])
+ll1 = sum(li[:len(li)//2])/len(li[:len(li)//2])
 ll2 = sum(li[(len(li)//2)+1:])/len(li[(len(li)//2)+1:])
 if ll1 == ll2:
     print("yes")
