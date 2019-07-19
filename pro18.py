91631c37f680fc3e92ffffb2a2d2b346d5e5cb3e
Gracekanagaraj <52075080+Gracekanagaraj@users.noreply.github.com>
2019-07-19 10:52:05 +0530

    Update pro18.py

--- pro18.py
+++ pro18.py
@@ -1,17 +1,17 @@
-n1,m1=map(int,input().split())
+arr1,brr1=map(int,input().split())
 l1=[]
-for _ in range(n1):
-	l1.append(input())
+for _ in range(arr1):
+    l1.append(input())
 for i in range(len(l1)):
-	if('0' in l1[i]):
-		l1[i]=l1[i].replace('0','')
-	l1[i]=l1[i].replace(' ','')
+    if('0' in l1[i]):
+        l1[i]=l1[i].replace('0','')
+    l1[i]=l1[i].replace(' ','')
 lengths=[]
 for i in l1:
-	if(len(i)>0):
-		lengths.append(len(i))
-m1=min(lengths)
-final_ans='11 '*m1
-final_ans=final_ans.strip()
-for i in range(m1):
-	print(final_ans)
+    if(len(i)>0):
+        lengths.append(len(i))
+brr1=min(lengths)
+r='11 '*brr1
+r=r.strip()
+for i in range(brr1):
+    print(r)
